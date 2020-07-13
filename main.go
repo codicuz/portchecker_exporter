6e1e5073c1ea344522c7823a616d52f8940ba463
Ваше Имя <you@example.com>
2020-07-13 08:55:43 +0700

    add only 80 port check

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ var (
 )
 
 const (
-    namespace = "was7"
+    namespace = "infra"
     exporter = "exporter"
 )
 
@@ -47,98 +47,18 @@ func main() {
 	host := os.Getenv("HOST_IP")
 	globalsleep := 15000 * time.Millisecond
 	log.Infoln("Hello, portChecker_exporter!")
-		
-	check1522port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check1522port_" + *wasInstance,
-            Help: "Check 1522 Oracle listener port",
-        })
-    prometheus.MustRegister(check1522port)
-	
-	check9043port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check9043port_" + *wasInstance,
-            Help: "Check 9043 WAS console port",
-        })
-    prometheus.MustRegister(check9043port)
-    
-    check9044port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check9044port_" + *wasInstance,
-            Help: "Check 9044 WAS console port",
-        })
-    prometheus.MustRegister(check9044port)
-    
-    check9045port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check9045port_" + *wasInstance,
-            Help: "Check 9045 WAS console port",
-        })
-	prometheus.MustRegister(check9045port)
-		
-	check10000port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check10000port_" + *wasInstance,
-            Help: "Check 10000 WAS console port",
-        })
-	prometheus.MustRegister(check10000port)
 
-	check10003port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check10003port_" + *wasInstance,
-            Help: "Check 10003 WAS console port",
-        })
-	prometheus.MustRegister(check10003port)
-
-	check10032port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check10032port_" + *wasInstance,
-            Help: "Check 10032 WAS console port",
-        })
-    prometheus.MustRegister(check10032port)
-
-	check10039port := prometheus.NewGauge (
-        prometheus.GaugeOpts {
-            Namespace: namespace,
-            Subsystem: exporter,
-            Name: "check10039port_" + *wasInstance,
-            Help: "Check 10039 Web Portal port",
-        })
-	prometheus.MustRegister(check10039port)
-	
-	check80port := prometheus.NewGauge (
+    check80port := prometheus.NewGauge (
         prometheus.GaugeOpts {
             Namespace: namespace,
             Subsystem: exporter,
             Name: "check80port_" + *wasInstance,
-            Help: "Check 80 IHS port",
+            Help: "Check 80 port",
         })
 	prometheus.MustRegister(check80port)
 	
 	go func () {
 		for {
-			raw_connect("tcp", host, "1522", check1522port)
-            raw_connect("tcp", host, "9043", check9043port)
-            raw_connect("tcp", host, "9044", check9044port)
-            raw_connect("tcp", host, "9045", check9045port)
-			raw_connect("tcp", host, "10000", check10000port)
-			raw_connect("tcp", host, "10003", check10003port)
-			raw_connect("tcp", host, "10032", check10032port)
-			raw_connect("tcp", host, "10039", check10039port)
 			raw_connect("tcp", host, "80", check80port)
 			time.Sleep(globalsleep)
 		}
